5820e8be937cd38c1c0ee9835054dece587cb7e5
Shafiyah Salsabillah <inishafiyah@gmail.com>
2023-03-29 21:40:43 +0700

    v1.2.1.

--- proses.php
+++ proses.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+// Ambil nilai dari textbox angka1 dan angka2
+$angka1 = $_POST["angka1"];
+$angka2 = $_POST["angka2"];
+
+// Ambil nilai dari dropdown operator
+$operator = $_POST["operator"];
+
+// Lakukan operasi matematika sesuai dengan nilai operator
+if ($operator == "+") {
+	$hasil = $angka1 + $angka2;
+} else if ($operator == "-") {
+	$hasil = $angka1 - $angka2;
+} else if ($operator == "*") {
+	$hasil = $angka1 * $angka2;
+} else if ($operator == "/") {
+	$hasil = $angka1 / $angka2;
+} else {
+	$hasil = "Error: Operator tidak valid";
+}
+
+// Tampilkan hasil operasi aritmatika
+echo "Hasil: " . $hasil;
+?>
